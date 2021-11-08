e9d3cb4edc2b3017b3725772c46b86e5538de34d
Nevindra <nevindraibnazhifi@gmail.com>
2021-11-08 14:41:59 +0700

    minor update for bug

--- src/parkings/parkings.routes.js
+++ src/parkings/parkings.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:id_user', controllers.getAllParkingTransactions);
-router.get('/:id_user/:id_parking')
+router.get('/:id_user/:id_parking', controllers.getSingleParkingTransactions)
 router.post('/', controllers.payParking)
 
 module.exports = router
