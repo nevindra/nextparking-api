1d92c5cc73a8bdcda1dd444f044d844d32d68a62
Nevindra <nevindraibnazhifi@gmail.com>
2021-12-16 18:09:50 +0700

    update parkings

--- src/parkings/parkings.routes.js
+++ src/parkings/parkings.routes.js
@@ -3,8 +3,8 @@ const express = require('express');
 const isAuth = require("../auth/auth.controllers");
 const router = express.Router();
 
-router.get('/:id_user', isAuth.verifyToken, controllers.getAllParkingTransactions);
-router.get('/:id_user/:id_parking', isAuth.verifyToken, controllers.getSingleParkingTransactions)
+router.get('/', isAuth.verifyToken, controllers.getAllParkingTransactions);
+router.get('/:id_parking', isAuth.verifyToken, controllers.getSingleParkingTransactions)
 router.post('/pay', isAuth.verifyToken, controllers.payParking)
 
 module.exports = router
